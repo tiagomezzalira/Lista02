31358584bc2790ba83cd705648f6ac38d578b92e
usuario <usuario@DESKTOP-CNTKCC8>
2022-02-08 14:35:47 -0300

    Add project files.

---
+++ Exercicio01.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio01.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numA, numB, numC;
+
+            Console.Write("Digite o número A: ");
+            numA = Convert.ToDouble(Console.ReadLine());
+            Console.Write(" Digite o número B: ");
+            numB = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o número C: ");
+            numC = Convert.ToDouble(Console.ReadLine());
+
+            if (numA + numB < numC)
+            {
+                Console.WriteLine("Sim,A + B é menor que C ! ! ! ");
+               
+            } else { Console.WriteLine("Não,A+B é maior que C ! ! !"); }
+            Console.ReadLine();
+           
+               
+        }
+    }
+}
